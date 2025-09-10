f3494b05478dc643eea2bb6adbd179e08578941f
jeysonprz <jassongeme23j@gmail.com>
2025-09-10 10:12:09 -0600

    Agrego ejercicio_e7.js

---
+++ ejercicio_e7.js
@@ -0,0 +1,25 @@
+// ejercico 1
+let agrearElemento = (arreglo, elementos) => {
+arreglo.push(elementos);
+return arreglo;
+}
+
+let numeros = [1, 2, 3];
+
+console .log(agrearElemento(numeros, 8));
+
+// ejercicios2
+let agrearElementoInicio = (arreglo, elementos ) => {
+    arreglo.unshift(elementos);
+     let numeros2 = 
+}
+
+// jercicio3
+let eliminarElementoFinal = (arreglo) => {
+    let ultimo = arreglo.pop();
+
+}
+
+
+let frutas = ['manzana', 'banana', 'naranja'];
+console.log(eliminarElementoFinal(frutas)); 
