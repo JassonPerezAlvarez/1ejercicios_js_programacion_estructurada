3413b9db1b5ceb710adf6e4932cd67731431ce8d
JassonPerezAlvarez <jassongeme23j@gmail.com>
2025-09-24 20:38:56 -0600

    ejercicios_e11

---
+++ ejercicios_e11.js
@@ -0,0 +1,97 @@
+
+let numeros = [5, 2, 9, 1, 7];
+
+numeros.sort((a, b) => a - b);
+console.log(numeros);
+
+numeros.sort((a, b) => b - a);
+console.log(numeros);
+
+
+let frutas = ["banana", "manzana", "naranja", "uva"];
+frutas.sort();
+console.log(frutas);
+
+
+frutas.sort((a, b) => b.localeCompare(a));
+console.log(frutas);
+
+ 
+
+
+let personas = [
+    { nombre: "Ana", edad: 28 },
+    { nombre: "Luis", edad: 22 },
+    { nombre: "Carlos", edad: 35 },
+    { nombre: "Marta", edad: 30 }
+];
+
+
+personas.sort((a, b) => a.edad - b.edad);
+console.log(personas);
+
+
+
+personas.sort((a, b) => a.nombre.localeCompare(b.nombre));
+console.log(personas);
+
+let numero = [1, 2, 3, 4, 5];
+numero.reverse();
+console.log(numero);
+
+let palabras = ["Hola", "Mundo", "JavaScript"];
+palabras.reverse();
+console.log(palabras);
+
+let persona = [
+    { nombre: "Ana", edad: 28 },
+    { nombre: "Luis", edad: 22 },
+    { nombre: "Carlos", edad: 35 },
+    { nombre: "Marta", edad: 30 }
+];
+
+persona.reverse();
+console.log(persona);
+
+
+ 
+
+
+let arreglo1 = [10, 3, 8, 1, 6];
+arreglo1.sort((a, b) => a - b);
+console.log(arreglo1); // [1, 3, 6, 8, 10]
+
+
+let arreglo2 = ["perro", "gato", "elefante", "ardilla"];
+arreglo2.sort();
+console.log(arreglo2);
+
+
+
+let arreglo3 = [
+    { nombre: "jose", puntaje: 85 },
+    { nombre: "maria", puntaje: 92 },
+    { nombre: "migdalia", puntaje: 78 }
+];
+arreglo3.sort((a, b) => b.puntaje - a.puntaje);
+console.log(arreglo3);
+
+
+
+let arreglo4 = [4, 9, 2, 7, 5];
+arreglo4.reverse();
+console.log(arreglo4); // [5, 7, 2, 9, 4]
+
+
+let arreglo5 = [
+    { producto: "computaddora", precio: 1200 },
+    { producto: "fon", precio: 800 },
+    { producto: "Tableb", precio: 600 }
+];
+arreglo5.sort((a, b) => a.precio - b.precio);
+console.log(arreglo5);
+
+
+let arreglo6 = ["manzana", "banana", "kiwi", "fresa"];
+arreglo6.reverse(); // c
+console.log(arreglo6);
