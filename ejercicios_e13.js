dd713c068d9a4de64310fcb449bca4a62bdd07e1
jeysonprz <jassongeme23j@gmail.com>
2025-10-01 10:38:00 -0600

    ejercicios e13

---
+++ ejercicios_e13.js
@@ -0,0 +1,107 @@
+const numero = [1, 2, 3, 4, 5];
+const suma = numeros.reduce((acc, num) => acc + num, 0);
+console.log(suma); // 15
+
+
+
+const productos = [
+{ nombre: "Camisa", precio: 20 },
+{ nombre: "Pantalón", precio: 30 },
+{ nombre: "Zapatos", precio: 50 }
+];
+const total = productos.reduce((acc, prod) => acc + prod.precio, 0);
+console.log(total); // 100
+
+
+
+
+const nombres = ["Ana", "Luis", "Pedro", "María"];
+const encontrado = nombres.find(nombre => nombre.startsWith("P"));
+console.log(encontrado); // "Pedro"
+
+
+
+const usuarios = [
+{ id: 1, nombre: "Carlos" },
+{ id: 2, nombre: "Lucía" },
+{ id: 3, nombre: "Marta" }
+];
+
+
+
+
+const usuario = usuarios.find(u => u.id === 2);
+console.log(usuario); // { id: 2, nombre: "Lucía" }
+
+
+
+
+const edades = [18, 22, 30, 25];
+const todosMayores = edades.every(edad => edad >= 18);
+console.log(todosMayores); // true
+
+
+
+
+const tareas = [
+{ nombre: "Estudiar", completada: true },
+{ nombre: "Ejercicio", completada: true },
+{ nombre: "Leer", completada: false }
+];
+const todasCompletas = tareas.every(t => t.completada === true);
+console.log(todasCompletas); // false
+
+const notas = [5, 8, 9, 3];
+const hayReprobados = notas.some(nota => nota < 6);
+console.log(hayReprobados); // true
+
+
+const inventario = [
+{ producto: "Manzanas", cantidad: 0 },
+{ producto: "Naranjas", cantidad: 10 },
+{ producto: "Peras", cantidad: 3 }
+];
+
+
+const agotado = inventario.some(item => item.cantidad === 0);
+console.log(agotado); // true
+
+
+
+
+const numeros = [5, 10, 15, 20, 25];
+const mayoresQue15 = numeros.filter(num => num > 15);
+console.log(mayoresQue15); // [20, 25]
+
+
+
+const empleados = [
+{ nombre: "Ana", puesto: "Desarrolladora" },
+{ nombre: "Luis", puesto: "Diseñador" },
+{ nombre: "Marta", puesto: "Desarrolladora" }
+];
+const desarrolladores = empleados.filter(e => e.puesto === "Desarrolladora");
+console.log(desarrolladores);
+// [
+// { nombre: "Ana", puesto: "Desarrolladora" },
+// { nombre: "Marta", puesto: "Desarrolladora" }
+// ]
+
+
+
+const personas = [
+  { id: 1, nombre: "Ana", edad: 17 },
+  { id: 2, nombre: "Luis", edad: 22 },
+  { id: 3, nombre: "Marta", edad: 30 },
+  { id: 4, nombre: "Pedro", edad: 15 },
+  { id: 5, nombre: "Sofía", edad: 25 },
+  { id: 6, nombre: "Diego", edad: 40 },
+  { id: 7, nombre: "Carla", edad: 19 },
+  { id: 8, nombre: "Jorge", edad: 33 },
+  { id: 9, nombre: "Lucía", edad: 28 },
+  { id: 10, nombre: "Raúl", edad: 16 },
+  { id: 11, nombre: "Elena", edad: 21 },
+  { id: 12, nombre: "Andrés", edad: 35 }
+];
+
+
